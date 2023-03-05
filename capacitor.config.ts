db7697fc43ad45b3715bcf6ff029efbb39f631e7
rajendra dhaka <rajendradhaka13@gmail.com>
2023-03-05 09:26:48 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'social-app-home',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
